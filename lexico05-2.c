e4ec8e820f9519f5cf5cd6cabb50edb5d47b3de2
nick <nick@nick-deb>
2018-04-16 16:39:42 -0300

    another exercise done

--- lexico05-2.c
+++ lexico05-2.c
@@ -42,6 +42,10 @@ void fill_letter(int len, int value, int array[len]) {
     fill_range(len, 'a', 'z', value, array);
 }
 
+void fill_line(int len, int value, int array[len]) {
+    fill_range(len, 0, 255, value, array);
+}
+
 unsigned char *reader() {
     unsigned char *str = empty_str();
     int c;
@@ -55,6 +59,21 @@ int main() {
     unsigned char *str = reader();
     int automata[13][LENGTH];
     int finals[] = {0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1};
+    char *final_names[] = {
+        "",
+        "ID",
+        "IF",
+        "ID",
+        "error",
+        "REAL",
+        "NUM",
+        "REAL",
+        "error",
+        "",
+        "comment",
+        "white space",
+        "error"
+    };
     int last_final, current_state;
     int start, upper, lower;
     int i = 0;
@@ -62,11 +81,97 @@ int main() {
     fill_array(13, LENGTH, -1, automata);
 
     // i = 0;
-    fill_letter(LENGTH, 3, &automata[i]);
-    fill_number(LENGTH, 6, &automata[i]);
+    fill_line(LENGTH, 12, automata[i]);
+    fill_letter(LENGTH, 3, automata[i]);
+    fill_number(LENGTH, 6, automata[i]);
     automata[i]['.'] = 4;
     automata[i]['i'] = 1;
+    automata[i]['-'] = 8;
+    automata[i][' '] = 11;
+    automata[i]['\t'] = 11;
 
+    i++; // i = 1;
+    fill_letter(LENGTH, 3, automata[i]);
+    fill_number(LENGTH, 3, automata[i]);
+    automata[i]['f'] = 2;
+
+    i++; // i = 2;
+    fill_letter(LENGTH, 3, automata[i]);
+    fill_number(LENGTH, 3, automata[i]);
+
+    i++; // i = 3;
+    fill_letter(LENGTH, 3, automata[i]);
+    fill_number(LENGTH, 3, automata[i]);
+
+    i++; // i = 4;
+    fill_number(LENGTH, 5, automata[i]);
+
+    i++; // i = 5;
+    fill_number(LENGTH, 5, automata[i]);
+
+    i++; // i = 6;
+    fill_number(LENGTH, 6, automata[i]);
+    automata[i]['.'] = 7;
+
+    i++; // i = 7;
+    fill_number(LENGTH, 7, automata[i]);
+    
+    i++; // i = 8;
+    automata[i]['-'] = 9;
+
+    i++; // i = 9;
+    fill_letter(LENGTH, 9, automata[i]);
+    automata[i]['\n'] = 10;
+
+    i++; // i = 10;
+
+    i++; // i = 11;
+    automata[i][' '] = 11;
+    automata[i]['\t'] = 11;
+
+    i++; // i = 12;
+
+    start = 0;
+
+    while (str[start]) {
+
+        upper = start;
+        lower = start;
+        current_state = 0;
+        last_final = -1;
+
+        while (1) {
+            unsigned char c = str[lower++];
+
+            if (!c || c == '\n' || automata[current_state][c] == -1) {
+                if ((c == '\n' || !c) && start == upper) {
+                    upper++;
+                    break;
+                } else if (automata[current_state][c] == -1) {
+                    if (last_final != 11) {
+                        for (i = start; i < upper; i++) {
+                            if (str[i] != '\n') {
+                                fputc(str[i], stdout);
+                            }
+                        }
+                        fputc(' ', stdout);
+                    }
+                    fprintf(stdout, "%s\n", final_names[last_final]);
+                    break;
+                }
+            }
+
+            current_state = automata[current_state][c];
+            if (finals[current_state]) {
+                last_final = current_state;
+                upper = lower;
+            }
+        }
+
+        start = upper;
+    }
+
+    free(str);
 
     return 0;
 }
